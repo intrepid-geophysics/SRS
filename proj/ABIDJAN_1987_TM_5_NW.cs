466a672b04b45182007df234b9e4a6cb87a9b05a
Difu Wang <difu@intrepid-geophysics.com>
2018-11-23 14:45:50 +1100

    add SRS abidjan 1987/TM 5 NW

---
+++ proj/ABIDJAN_1987_TM_5_NW.cs
@@ -0,0 +1,19 @@
+CoordinateSystem Begin
+	POSCCode	= 2165
+	POSCName	= "Abidjan 1987 / TM 5 NW"
+	Projection Begin
+		ProjectionType	= TransverseMercator
+		CentralMeridian	= -3
+		LatOrigin	= 0
+		FalseEasting	= 500000
+		FalseNorthing	= 0
+		ScaleFactor	= 0.9996
+		POSCCode	= 17005
+		POSCName	= "TM 5 NW"
+		POSCMethod	= "Transverse Mercator"
+	Projection End
+	Datum Begin
+		POSCName	= "Abidjan 1987"
+		POSCCode	= "6143"
+	Datum End
+CoordinateSystem End
