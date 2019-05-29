f21b0ac80c79314001cf01d03a2c065b788e82b6
grcgit <30640935+grcgit@users.noreply.github.com>
2019-05-29 15:02:47 +1000

    Added GDA2020 datum and mga 46-59 projections

---
+++ proj/GDA2020.cs
@@ -0,0 +1,8 @@
+CoordinateSystem Begin
+	POSCCode	= 7844
+	POSCName	= "GDA2020"
+	Datum Begin
+		POSCName	= "Geocentric Datum of Australia 2020"
+		POSCCode	= "1168"
+	Datum End
+CoordinateSystem End
---
+++ proj/GDA2020_MGA_ZONE_55.cs
@@ -0,0 +1,19 @@
+CoordinateSystem Begin
+	POSCCode	= 7855
+	POSCName	= "GDA2020 / MGA zone 55"
+	Projection Begin
+		ProjectionType	= TransverseMercator
+		CentralMeridian	= 147
+		LatOrigin	= 0
+		FalseEasting	= 500000
+		FalseNorthing	= 10000000
+		ScaleFactor	= 0.9996
+		POSCCode	= 17355
+		POSCName	= "Map Grid of Australia zone 55"
+		POSCMethod	= "Transverse Mercator"
+	Projection End
+	Datum Begin
+		POSCName	= "Geocentric Datum of Australia 2020"
+		POSCCode	= "1168"
+	Datum End
+CoordinateSystem End
